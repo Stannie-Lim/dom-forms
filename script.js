2012b6d8367b02b08a7bc01b73127439b6f44d15
Stanley <lims95@yahoo.com>
2020-01-25 12:21:23 -0500

    first commit

--- script.js
+++ script.js
@@ -5,16 +5,44 @@ const customers = [
 ];
 
 const list = document.querySelector('ul');
+const button = document.querySelector("button");
+const form = document.querySelector("form");
 
+const sortArray = () => {
+    customers.forEach(customer => {
+        customer.dateJoined = new Date(customer.dateJoined);
+    });
+    customers.sort((a, b) => a.dateJoined - b.dateJoined);
+}
+
+
+let id = 4;
+button.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const nameInput = form.querySelector("[name='name']");
+    const dateOf = form.querySelector("[name='date']").value;
+    const dateOfHireInput = new Date(dateOf);
+    const vipOrNot = form.querySelector("[name='vip']").checked;
+    customers.push({ 
+        id: id,
+        name: nameInput.value,
+        email: nameInput.value + '@gmail.com',
+        dateJoined: dateOfHireInput,
+        isVIP: vipOrNot
+    });
+
+    id++;
+    sortArray();
+    render();
+});
+sortArray();
 
 const render = () => {
     const html = customers.map(customer => {
-        const dateOfHire = customer.dateJoined;
-        const date = new Date(dateOfHire);
-        const diff = new Date() - date;
+        const diff = new Date() - customer.dateJoined;
         const msInYear = 1000*60*60*24*365;
         const years = (diff / msInYear).toFixed(2);
-        return `<li class='${ customer.isVIP ? 'vip': ''}'>${customer.name} joined on ${customer.dateJoined} and has been a member for ${years} years.</li>`;
+        return `<li class='${ customer.isVIP ? 'vip': ''}'>${customer.name} joined on ${customer.dateJoined.toLocaleDateString()} and has been a member for ${years} years.</li>`;
     }).join('');
     list.innerHTML = html;
 };
